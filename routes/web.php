4af2c5fe8ee80603ef3de7aca9219a26caf54eab
ismaelgiovannetti <ismael.giovannetti@heig-vd.ch>
2025-06-19 06:57:12 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,40 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 // Public routes
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 // Authentication Routes
-Route::get('/login', [\App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'create'])
-    ->middleware('guest')
-    ->name('login');
-
-Route::post('/login', [\App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'store'])
-    ->middleware('guest');
-
-Route::post('/logout', [\App\Http\Controllers\Auth\AuthenticatedSessionController::class, 'destroy'])
-    ->middleware('auth')
-    ->name('logout');
+Route::middleware('guest')->group(function () {
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])
+        ->name('login');
+    
+    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+});
 
 // Protected routes
-Route::middleware(['auth'])->group(function () {
-    // Add your protected routes here
+Route::middleware('auth')->group(function () {
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+        ->name('logout');
+
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+    
+    // Add more protected routes here as needed
 });
