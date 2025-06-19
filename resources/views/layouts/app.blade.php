4f0a8f61fd5514ee5fdec690e5d6a8aa3325cbc8
ismaelgiovannetti <ismael.giovannetti@heig-vd.ch>
2025-06-19 22:10:56 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,16 +27,13 @@
                 <ul class="navbar-nav me-auto">
                     @auth
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('dashboard') }}">Dashboard</a>
+                            <a class="nav-link" href="{{ route('home') }}">Invoices</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('products.index') }}">Products</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('clients.index') }}">Clients</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('invoices.index') }}">Invoices</a>
                         </li>
                     @endauth
                 </ul>
